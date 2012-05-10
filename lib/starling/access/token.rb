e2d351f12a513928e7c78d827d60d282b6d0e165
Jeremy Bush <contractfrombelow@gmail.com>
2012-05-10 08:18:56 -0500

    Flesh out access token for a better api

--- lib/starling/access/token.rb
+++ lib/starling/access/token.rb
@@ -6,11 +6,21 @@ module Starling
       end
 
       def get_token
-        eval @store.string
+        return parse_stored_token if has_token?
       end
 
       def save_token(token)
         @store.puts token
+      end
+
+      def has_token?
+        parse_stored_token.is_a?(Hash) && parse_stored_token.has_key?(:token) && parse_stored_token.has_key?(:secret)
+      end
+
+      private
+
+      def parse_stored_token
+        eval @store.string
       end
     end
   end
--- spec/access_token_spec.rb
+++ spec/access_token_spec.rb
@@ -1,12 +1,29 @@
 require 'starling/access/token.rb'
 
 describe Starling::Access::Token do
+  it 'knows when it has a valid access token' do
+    store = StringIO.new("{:token => 'foo', :secret => 'bar'}")
+    token = Starling::Access::Token.new(store)
+    token.has_token?.should be_true
+  end
+
+  it 'knows when it has an invalid access token' do
+    store = StringIO.new
+    token = Starling::Access::Token.new(store)
+    token.has_token?.should be_false
+  end
+
   it 'loads an access token when it exists' do
     store = StringIO.new("{:token => 'foo', :secret => 'bar'}")
     token = Starling::Access::Token.new(store).get_token
     token.should be_a(Hash)
     token[:token].should == 'foo'
     token[:secret].should == 'bar'
+  end
+
+  it 'does not bomb when loading an unexpected type from the datastore' do
+    store = StringIO.new("'foobar'")
+    Starling::Access::Token.new(store).get_token
   end
 
   it 'fails to load an access token when it does not exist' do
