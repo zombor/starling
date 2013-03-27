f4f121e57cc18ff8f29a7238c58ddf2c136cc7e0
Jeremy Bush <contractfrombelow@gmail.com>
2013-03-27 07:27:10 -0500

    Pass stuff into constructor of delivery, not execute

--- lib/starling/delivery/cli.rb
+++ lib/starling/delivery/cli.rb
@@ -3,30 +3,34 @@ require 'eventmachine'
 module Starling
   module Delivery
     class CLI
-      def initialize(config)
+      def initialize(config, timeline, twitter, command, output)
         @config = config
+        @timeline = timeline
+        @twitter = twitter
+        @command = command
+        @output = output
       end
 
-      def execute(timeline, twitter, command, output)
+      def execute
         EM.run do
           Thread.start do
             while buf = Readline.readline("> ", true)
               if buf == 'stop'
-                timeline.stop
+                @timeline.stop
               else
-                command.process(buf.chomp)
+                @command.process(buf.chomp)
               end
             end
           end
 
           EM.add_periodic_timer(1) do
-            output.output(timeline.output)
+            @output.output(@timeline.output)
 
-            timeline.output.clear
+            @timeline.output.clear
             Readline.refresh_line
           end
 
-          timeline.latest
+          @timeline.latest
         end
       end
     end
